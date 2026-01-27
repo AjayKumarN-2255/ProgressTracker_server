725d19658057b8cd2b7fcd2f053aac22deb2536a
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-01-27 15:05:49 +0530

    added qeuery based fetch in get all projcect function

--- controllers/project/getAllProject.js
+++ controllers/project/getAllProject.js
@@ -3,8 +3,10 @@ const Project = require('../../models/Project');
 
 async function getAllProjects(req, res, next) {
 
+    const { pid } = req.query;
+    const query = pid ? { _id: pid } : {};
     try {
-        const projects = await Project.find({}, '_id name from to');
+        const projects = await Project.find(query, '_id name from to');
         res.status(STATUS_CODES.CREATED).json({
             success: true,
             message: 'Project fetched successfully',
