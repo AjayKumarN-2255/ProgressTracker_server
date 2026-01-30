6400a61d5f267b4a0afecee0bd5148d1a2ebdc34
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-01-30 16:39:16 +0530

    edited delted project api function

--- services/project/deleteProject.js
+++ services/project/deleteProject.js
@@ -1,8 +1,20 @@
 const { APIError } = require('../../shared/error/APIError');
 const { STATUS_CODES } = require('../../shared/constants/statusCodes');
 const Project = require('../../models/Project');
+const Review = require('../../models/Review');
 
 async function deleteProject(pId) {
+    const reviews = await Review.find({
+        project: pId
+    });
+
+    if (reviews.length > 0) {
+        throw new APIError(
+            STATUS_CODES.FORBIDDEN,
+            "Cannot delete project because reviews exist"
+        );
+    }
+
     const project = await Project.findById(pId);
     if (!project) {
         throw new APIError(STATUS_CODES.NOT_FOUND, "Project is not exist");
