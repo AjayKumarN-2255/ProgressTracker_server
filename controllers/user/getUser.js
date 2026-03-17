642244fdd2592fb72eddf524ce1ac25fff4cea2f
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-03-17 10:53:24 +0530

    updated get user api , for selected fields and get user by Id

--- controllers/user/getUser.js
+++ controllers/user/getUser.js
@@ -1,12 +1,37 @@
 const { STATUS_CODES } = require('../../shared/constants/statusCodes');
+const { APIError } = require('../../shared/error/APIError');
 const User = require('../../models/User');
 
 async function getUser(req, res, next) {
     try {
-        const { role } = req.query;
+        const { role, reqField, userId } = req.query;
         const filter = role ? { role } : {};
 
-        const users = await User.find(filter, '_id name');
+
+        const allowedFields = ['_id', 'name', 'email', 'role', 'designation', 'profile'];
+        let selectedFields = '_id name';
+
+        if (reqField) {
+            const requested = reqField.split(' ');
+            const filtered = requested.filter(f => allowedFields.includes(f));
+            if (filtered.length) {
+                selectedFields = filtered.join(' ');
+            }
+        }
+
+        if (userId) {
+            const user = await User.findById(userId, selectedFields);
+            if (!user) {
+                return next(new APIError(STATUS_CODES.NOT_FOUND, "user not exist"));
+            }
+            return res.status(STATUS_CODES.OK).json({
+                success: true,
+                message: "user detail fetched successfully",
+                data: user,
+            });
+        }
+        
+        const users = await User.find(filter, selectedFields);
 
         return res.status(STATUS_CODES.OK).json({
             success: true,
